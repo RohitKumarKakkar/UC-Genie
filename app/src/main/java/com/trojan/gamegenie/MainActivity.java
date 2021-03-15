7f8a04c38d926959237038f2fe3956fb2106ce71
RohitKumarKakkar <zkumarzrohit@gmail.com>
2021-03-15 16:47:55 +0530

    UC GENIE Updated

--- app/src/main/java/com/trojan/gamegenie/MainActivity.java
+++ app/src/main/java/com/trojan/gamegenie/MainActivity.java
@@ -1,74 +1,146 @@
 package com.trojan.gamegenie;
 
-import androidx.annotation.NonNull;
-import androidx.appcompat.app.ActionBarDrawerToggle;
+import androidx.annotation.ColorInt;
+import androidx.annotation.ColorRes;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
-import androidx.drawerlayout.widget.DrawerLayout;
+import androidx.core.content.ContextCompat;
+import androidx.fragment.app.Fragment;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
+import android.content.res.TypedArray;
+import android.graphics.drawable.Drawable;
 import android.os.Bundle;
-import android.view.Menu;
-import android.view.MenuItem;
-import android.view.View;
-import android.view.WindowManager;
+import android.widget.TextView;
 
-import com.google.android.material.navigation.NavigationView;
 import com.trojan.gamegenie.Fragments.HomeFragment;
+import com.trojan.gamegenie.SlidingNavBar.DrawerAdapter;
+import com.trojan.gamegenie.SlidingNavBar.DrawerItem;
+import com.trojan.gamegenie.SlidingNavBar.SimpleItem;
+import com.trojan.gamegenie.SlidingNavBar.SpaceItem;
+import com.yarolegovich.slidingrootnav.SlidingRootNav;
+import com.yarolegovich.slidingrootnav.SlidingRootNavBuilder;
 
-public class MainActivity extends AppCompatActivity implements NavigationView.OnNavigationItemSelectedListener {
-    private DrawerLayout drawer;
+import java.util.Arrays;
+
+public class MainActivity extends AppCompatActivity implements DrawerAdapter.OnItemSelectedListener {
+    private static final int POS_DASHBOARD = 0;
+    private static final int POS_ACCOUNT = 1;
+    private static final int POS_REDEEM = 2;
+    private static final int POS_LUCKY_SPIN = 3;
+    private static final int POS_MATCH_AND_EARN = 5;
+    private static final int POS_LOGOUT = 6;
+
+    private String[] screenTitles;
+    private Drawable[] screenIcons;
+
+    TextView tvBalance;
+
+    private SlidingRootNav slidingRootNav;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        getWindow().setSoftInputMode(
-                WindowManager.LayoutParams.SOFT_INPUT_STATE_ALWAYS_HIDDEN);
-
         Toolbar toolbar = findViewById(R.id.toolbarid);
         setSupportActionBar(toolbar);
         getSupportActionBar().setTitle(null);
-////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
-        //Navigation Drawer Coding
-        drawer = findViewById(R.id.drawer_layout);
-        NavigationView navigationView = findViewById(R.id.nav_view);
-        navigationView.setNavigationItemSelectedListener(MainActivity.this);
 
-        View headerView = navigationView.getHeaderView(0);
+        slidingRootNav = new SlidingRootNavBuilder(this)
+                .withToolbarMenuToggle(toolbar)
+                .withMenuOpened(false)
+                .withContentClickableWhenMenuOpened(false)
+                .withSavedState(savedInstanceState)
+                .withMenuLayout(R.layout.menu_left_drawer)
+                .inject();
 
-        ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(this, drawer, toolbar,
-                R.string.navigation_drawer_open, R.string.navigation_drawer_close);
+        screenIcons = loadScreenIcons();
+        screenTitles = loadScreenTitles();
 
-        drawer.addDrawerListener(toggle);
-        toggle.syncState();
+        DrawerAdapter adapter = new DrawerAdapter(Arrays.asList(
+                createItemFor(POS_DASHBOARD).setChecked(true),
+                createItemFor(POS_ACCOUNT),
+                createItemFor(POS_REDEEM),
+                createItemFor(POS_LUCKY_SPIN),
+                new SpaceItem(48),
+                createItemFor(POS_MATCH_AND_EARN),
+                createItemFor(POS_LOGOUT)));
+        adapter.setListener(MainActivity.this);
 
+        RecyclerView list = findViewById(R.id.list);
+        list.setNestedScrollingEnabled(false);
+        list.setLayoutManager(new LinearLayoutManager(this));
+        list.setAdapter(adapter);
 
-        //Saved Instance For Rotating The Phone.
-        if (savedInstanceState == null) {
-            getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container, new HomeFragment()).commit();
-            navigationView.setCheckedItem(R.id.nav_home);
+        tvBalance = findViewById(R.id.tvBalance);
+        tvBalance.setText("1000");
+
+        adapter.setSelected(POS_DASHBOARD);
+    }
+
+    @Override
+    public void onItemSelected(int position) {
+        if (position == POS_LOGOUT) {
+            finish();
+        } else if (position == POS_DASHBOARD) {
+            Fragment selectedScreen = new HomeFragment();
+            showFragment(selectedScreen);
+        } else if (position == POS_ACCOUNT) {
+            Fragment selectedScreen = new HomeFragment();
+            showFragment(selectedScreen);
+        } else if (position == POS_REDEEM) {
+            Fragment selectedScreen = new HomeFragment();
+            showFragment(selectedScreen);
+        } else if (position == POS_LUCKY_SPIN) {
+            Fragment selectedScreen = new HomeFragment();
+            showFragment(selectedScreen);
+        } else if (position == POS_MATCH_AND_EARN) {
+            Fragment selectedScreen = new HomeFragment();
+            showFragment(selectedScreen);
         }
 
+        slidingRootNav.closeMenu();
+
     }
 
-    @Override
-    public boolean onNavigationItemSelected(@NonNull MenuItem item) {
-        return false;
+    private void showFragment(Fragment fragment) {
+        getSupportFragmentManager().beginTransaction()
+                .replace(R.id.container, fragment)
+                .commit();
     }
 
-    @Override
-    public boolean onCreateOptionsMenu(Menu toolbar_menu) {
-        getMenuInflater().inflate(R.menu.toolbar_menu, toolbar_menu);
-        return super.onCreateOptionsMenu(toolbar_menu);
+    @SuppressWarnings("rawtypes")
+    private DrawerItem createItemFor(int position) {
+        return new SimpleItem(screenIcons[position], screenTitles[position])
+                .withIconTint(color(R.color.textColorSecondary))
+                .withTextTint(color(R.color.textColorPrimary))
+                .withSelectedIconTint(color(R.color.colorAccent))
+                .withSelectedTextTint(color(R.color.colorAccent));
     }
 
-    ////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle item selection
-        switch (item.getItemId()) {
+    private String[] loadScreenTitles() {
+        return getResources().getStringArray(R.array.ld_activityScreenTitles);
+    }
+
+    private Drawable[] loadScreenIcons() {
+        TypedArray ta = getResources().obtainTypedArray(R.array.ld_activityScreenIcons);
+        Drawable[] icons = new Drawable[ta.length()];
+        for (int i = 0; i < ta.length(); i++) {
+            int id = ta.getResourceId(i, 0);
+            if (id != 0) {
+                icons[i] = ContextCompat.getDrawable(this, id);
+            }
         }
-        return true;
+        ta.recycle();
+        return icons;
     }
+
+    @ColorInt
+    private int color(@ColorRes int res) {
+        return ContextCompat.getColor(this, res);
+    }
+
+
 }
